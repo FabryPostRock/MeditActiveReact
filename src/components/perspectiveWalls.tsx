6fe6b14b34a149d735ab80cc9a4cad6d482503b7
Fabri <fabriziodemasi01.com>
2026-09-21 17:55:43 +0200

    added opacity to background image and deleted useless images

--- src/components/perspectiveWalls.tsx
+++ src/components/perspectiveWalls.tsx
@@ -3,7 +3,7 @@ import perspectiveWalls from '../assets/img/background_bordi_zen_fiori_di_loto_c
 export function PerspectiveWalls() {
   return (
     <div className="perspective-walls" aria-hidden="true">
-      <img className="perspective-walls__image" src={perspectiveWalls} alt="" draggable={false} />
+      <img className="opacity-50 perspective-walls__image" src={perspectiveWalls} alt="" draggable={false} />
     </div>
   );
 }
